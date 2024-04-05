f9215d6a5f4df6bd3c44a4ed04860f0568ba6e58
CG <coco241200@gmail.com>
2024-04-05 13:14:58 +0200

    commune: formatage et classe

--- partie1/__init__.py
+++ partie1/__init__.py
@@ -1,21 +1,39 @@
 import requests
 
 class Commune:
-    nom: str
-    code: str
-    deptCode: str
-    siren: str
-    codeEpci: str
-    codeRegion: str
-    codesPost: list[str]
-    population: int
-    score: int
-    search_url: str
+    nom: str = ""
+    code: str = ""
+    deptCode: str = ""
+    siren: str = ""
+    codeEpci: str = ""
+    codeRegion: str = ""
+    codesPostaux: list[str] = []
+    population: int = 0
+    score: int = 0
+    search_url: str = ""
+
+    def __init__(self, json_dict: dict={}):
+        if len(json_dict) != 0:
+            self.nom = json_dict["nom"]
+            self.code = json_dict["code"]
+            self.deptCode = json_dict["codeDepartement"]
+            self.siren = json_dict["siren"]
+            self.codeEpci = json_dict["codeEpci"]
+            self.codeRegion = json_dict["codeRegion"]
+            self.codesPostaux = json_dict["codesPostaux"]
+            self.population = json_dict["population"]
+            if "score" in json_dict:
+                self.score = json_dict["score"]
+
+    def __str__(self):
+        return f"Commmune(\n\tnom: {self.nom}\n\tcode: {self.code}\n\tdeptCode: {self.deptCode}\n\tsiren: {self.siren}\n\tcodeEpci: {self.codeEpci}\n\tcodeRegion: {self.codeRegion}\n\tcodesPostaux: {self.codesPostaux}\n\tpopulation: {self.population}\n\tscore: {self.score}\n\tsearch_url: {self.search_url}\n)"
 
     def search(postalCode: str):
         default_url = "https://geo.api.gouv.fr/communes/"
         result = requests.get(default_url, params={"codePostal": postalCode})
         if result.status_code != 200:
             print("request went wrong")
-            return []
-        return result.json()
+            return
+        commune = Commune(result.json()[0])
+        commune.search_url = result.url
+        return commune
