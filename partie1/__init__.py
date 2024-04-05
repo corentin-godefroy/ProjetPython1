843a0526d17005d431d83f7d571d8c3000ba3923
CG <coco241200@gmail.com>
2024-04-05 12:29:25 +0200

    recherche communes

--- partie1/__init__.py
+++ partie1/__init__.py
@@ -12,28 +12,10 @@ class Commune:
     score: int
     search_url: str
 
-    def from_json(self, json: dict):
-        return
-
-    def search(self, postalCode: str):
+    def search(postalCode: str):
         default_url = "https://geo.api.gouv.fr/communes/"
-        result = requests.get(default_url, params=postalCode)
+        result = requests.get(default_url, params={"codePostal": postalCode})
         if result.status_code != 200:
             print("request went wrong")
-            return {}
-        return json.loads(result.content)["items"]
-
-    'https://geo.api.gouv.fr/communes?codePostal=78000'
-
-class Departement:
-    communes: list[Commune]
-    search_url: str
-
-    def search(self):
-        default_url = "https://geo.api.gouv.fr/departements/"
-        
-    def get_population_totale(self) -> int:
-        total = 0
-        for commune in self.communes:
-            total += commune.population
-        return total
+            return []
+        return result.json()
