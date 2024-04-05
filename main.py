bb79ddacd77268b1316824ad5f12876b12d9d597
CG <coco241200@gmail.com>
2024-04-05 14:27:25 +0200

    optimisation main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from partie1 import Commune
 from partie1 import Departement
 
 print("----- Partie 1 -----")
-input = input("Tapez le numéro du code postal ou du département recherché : ")
+input = input("Tapez le nom de la commune ou le numéro du code postal ou du département recherché : ")
 
-if len(input) < 4:
+if input.isnumeric() and len(input) < 4:
     Departement.seach(input).print_population()
 else:
     Commune.search(input).print_population()
